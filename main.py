92fe5b037424584f2a87b93b2703442b3a51a04d
Your Name <you@example.com>
2025-05-31 11:02:10 -0700

    Removed the old google folder, we now have calendar, added tags to the docs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from apis.weather.routes import router as weather_router
-from apis.google.routes import router as google_router
 from apis.locations.routes import router as locations_router
 from apis.health.routes import router as health_router
 from apis.calendar import routes as calendar_routes
@@ -24,11 +23,10 @@ app.add_middleware(
 )
 
 # Include routers with prefixes and API key dependency
-app.include_router(weather_router, prefix="/weather", dependencies=[Depends(get_api_key)])
-app.include_router(google_router, prefix="/google", dependencies=[Depends(get_api_key)])
-app.include_router(locations_router, prefix="/locations", dependencies=[Depends(get_api_key)])
-app.include_router(health_router, prefix="/health", dependencies=[Depends(get_api_key)])
-app.include_router(calendar_routes.router, prefix="/calendar", tags=["calendar"], dependencies=[Depends(get_api_key)])
+app.include_router(weather_router, prefix="/weather", tags=["Weather"], dependencies=[Depends(get_api_key)])
+app.include_router(locations_router, prefix="/locations", tags=["Location"], dependencies=[Depends(get_api_key)])
+app.include_router(health_router, prefix="/health", tags=["Health"], dependencies=[Depends(get_api_key)])
+app.include_router(calendar_routes.router, prefix="/calendar", tags=["Calendar"], dependencies=[Depends(get_api_key)])
 
 
 @app.get("/")
